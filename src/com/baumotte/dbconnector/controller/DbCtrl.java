cdc5e76dfe82dcac4f40e1d4621e79d1232ac38e
baumotte <moritz@baumotte.com>
2018-10-17 14:09:42 +0200

    getting responses works

--- src/com/baumotte/dbconnector/controller/DbCtrl.java
+++ src/com/baumotte/dbconnector/controller/DbCtrl.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import com.baumotte.dbconnector.entities.Response;
 import com.baumotte.dbconnector.entities.Ticket;
 
 public class DbCtrl {
@@ -16,13 +17,20 @@ public class DbCtrl {
 	private final String DB_CONNECTION = "jdbc:mysql://localhost:3306/com.baumotte?useSSL=false";
 	private final String DB_USER = "root";
 	private final String DB_PASSWORD = "Fussel01";
-		
+	
+	/**
+	 * Used to get tickets by email
+	 * @param email
+	 * @return ArrayList<Ticket>
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
 	public ArrayList<Ticket> getTickets(String email) throws SQLException, ClassNotFoundException {
 		ArrayList<Ticket> tickets = new ArrayList<>();
 		String query = "select id, email, title, text from tickets where email = ?;";
 		
 		Class.forName(DB_DRIVER);
-
+		
 		Connection dbConnection = getDBConnection();
 		PreparedStatement prepStmt = dbConnection.prepareStatement(query);
 		prepStmt.setString(1, email);
@@ -40,7 +48,43 @@ public class DbCtrl {
 		
 		return tickets;
 	}
+
+	/**
+	 * Used to get responses for a specific ticket
+	 * @param ticketID
+	 * @return ArrayList<Response>
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public ArrayList<Response> getResponses(int ticketID) throws SQLException, ClassNotFoundException {
+		ArrayList<Response> responses = new ArrayList<>();
+		String query = "select id, response_text, ticket_id from responses where ticket_id = ?;";
+		
+		Class.forName(DB_DRIVER);
+		
+		Connection dbConnection = getDBConnection();
+		PreparedStatement prepStmt = dbConnection.prepareStatement(query);
+		prepStmt.setInt(1, ticketID);
 	
+		ResultSet rs = prepStmt.executeQuery();
+		
+		while(rs.next()) {
+			responses.add(new Response(
+						rs.getInt("id"),
+						rs.getString("response_text"),
+						rs.getInt("ticket_id")
+					));
+		}
+		
+		return responses;
+	}
+	
+	/**
+	 * Used to get database connection instance
+	 * @return DBconnection
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
 	private Connection getDBConnection() throws SQLException, ClassNotFoundException {
 		Connection dbConnection = null;
 		Class.forName(DB_DRIVER);
--- src/com/baumotte/dbconnector/endpoints/TicketingDbEP.java
+++ src/com/baumotte/dbconnector/endpoints/TicketingDbEP.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -22,14 +23,33 @@ public class TicketingDbEP {
 	}
 
 	@GET
+	@Path ("/tickets")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getQueryResults(@QueryParam("user") String email) {
+	public Response getTickets(@QueryParam("user") String email) {
 		Response r = null;
 		try {
 			r = Response
 					.status(Response.Status.OK)
 					.entity(dbCtrl.getTickets(email))
+					.build();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return r;
+	}
+	
+	@GET
+	@Path ("/tickets/{id}/responses")
+	@Produces (MediaType.APPLICATION_JSON)
+	public Response getResponses(@PathParam("id") int id) {
+		Response r = null;
+		try {
+			r = Response
+					.status(Response.Status.OK)
+					.entity(dbCtrl.getResponses(id))
 					.build();
 		} catch (SQLException e) {
 			e.printStackTrace();
